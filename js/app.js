878361abe2f57412d09d72957258283e5a3d3c3e
LauraJH16 <laurajimenezh16@hotmail.com>
2018-01-09 06:03:27 -0500

    Arreglando html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
   $('.carousel.carousel-slider').carousel({fullWidth: true});
-  $(".button-collapse").sideNav();
+  $('.button-collapse').sideNav({
+    menuWidth: 200, // Default is 300
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true, // Choose whether you can drag to open on touch screens,
+    onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+    onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed*/
+  });
+
+  var config = {
+    apiKey: "AIzaSyADv1fxkUiJ1l7qjDsigV2MyCQokrarJVY",
+    authDomain: "sign-c5945.firebaseapp.com",
+    databaseURL: "https://sign-c5945.firebaseio.com",
+    projectId: "sign-c5945",
+    storageBucket: "sign-c5945.appspot.com",
+    messagingSenderId: "21338253193"
+  };
+  firebase.initializeApp(config);
+  
+
 });
