af34364c2eaba280c417b7f02d20aa84eec94efe
LauraJH16 <laurajimenezh16@hotmail.com>
2018-01-15 09:31:30 -0500

    Talent Fest

--- js/login.js
+++ js/login.js
@@ -138,7 +138,8 @@ $(document).ready(function() {
     var reader = new FileReader();
 
     reader.onload = function(e) {
-      $('.content-text').append('<div class="posteando card flow-text"> <img class="img-file img-post center-block" src="#"> <br><br><span class="grey-text">Publicado a las :' + getTime() + '</span><br><br><a href="#!" class="secondary-content"><i class="material-icons">favorite_border</i></a></div></div>');
+      $('.content-text').append('<div class="col l10 offset-l1 posteando card flow-text s12"> <img class="img-file img-post center-block" src="#"> <br><br><span class="grey-text">Publicado a las :' + getTime() + '</span><br><a href="#!" class="secondary-content"><i class="material-icons">favorite_border</i></a></div></div>');
+      $('#test4').append('<div class="col l10 offset-l1 posteando card flow-text s12"> <img class="img-file img-post center-block" src="#"> <br><br><span class="grey-text">Publicado a las :' + getTime() + '</span><br><a href="#!" class="secondary-content"><i class="material-icons">favorite_border</i></a></div></div>');
       $('.img-post').attr('src', e.target.result);
     };
 
@@ -168,16 +169,14 @@ $(document).ready(function() {
 
   $('.postext').on('click', function() {
     var $content = $('#textarea1').val();
-    $('.content-text').append('<div class="col l10 offset-l1 posteando card flow-text">' + $content + '<br><br><span class="grey-text">Publicado a las :' + getTime() + '</span><br><a href="#!" class="secondary-content like"><i class="material-icons">favorite_border</i><span class="count">0</span></a></div>');
-    $('#test4').append('<div class="posteando card flow-text col s12">' + $content + '<br><br><span class="grey-text">Publicado a las :' + getTime() + '</span><br><br></div>');
+    $('.content-text').append('<div class="col l10 offset-l1 posteando card flow-text s12">' + $content + '<br><br><span class="grey-text">Publicado a las :' + getTime() + '</span><br><a href="#!" class="secondary-content like"><i id="heart" class="material-icons">favorite_border</i></a></div>');
+    $('#test4').append('<div class="col l10 offset-l1 posteando card flow-text s12">' + $content + '<br><br><span class="grey-text">Publicado a las :' + getTime() + '</span><br><a href="#!" class="secondary-content like"><i id="heart" class="material-icons">favorite_border</i></a></div>');
     $('#textarea1').val('');
+    $('.like i').click(function() {
+      $(this).text('favorite');
+    });
   });
-
-  $('.like').click(function() {
-    alert('aaa');
-    var count = 0;
-    $('.count').text(count++);
-  });
+ 
 
   function getTime() {
     var currentDate = new Date();
